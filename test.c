270072ec3f1374a1acf45380af83bb5735120501
Louan <90987010+blupblup46@users.noreply.github.com>
2021-12-20 10:33:51 +0100

    Add files via upload
    Une bonne envie de commiter

--- test.c
+++ test.c
@@ -4,74 +4,107 @@
 #include <string.h>
 #include <time.h>
 
-#define LIGNE 10
-#define CARAC 5
+const int LIGNE=50;
+const int CARAC=10;
 
-void afficheTab(char tab[LIGNE][CARAC]);
-void echange (char a[CARAC], char b[CARAC]);
-void Tri_bulle_Croissant(char t[LIGNE][CARAC]);
-void initTab(char t[LIGNE][CARAC]);
+void afficheTab(char tab[LIGNE][CARAC+1]);
+void echange (char a[CARAC], char b[CARAC+1]);
+void Tri_bulle_Croissant_nbCaract(char t[LIGNE][CARAC+1]);
+void Tri_bulle_Croissant_alpha(char t[LIGNE][CARAC+1]);
+void initTab(char t[LIGNE][CARAC+1]);
 
 int main(){
-    char tab[LIGNE][CARAC];
+    int boucle;
+    char tab[LIGNE][(CARAC+1)];
     srand(time(NULL));
+
 
     initTab(tab);
     afficheTab(tab);
-    Tri_bulle_Croissant(tab);
+    putchar('\n');
+    
+    Tri_bulle_Croissant_nbCaract(tab);
     printf("\n");
     afficheTab(tab);
 }
 
-void afficheTab(char tab[LIGNE][CARAC]){
-    int ligne, car;
-    for (ligne=0; ligne< LIGNE; ligne++){
-        printf("**********LIGNE %d**********\n", ligne);
-        for (car=0; car<CARAC; car++){
-            printf("%c", tab[ligne][car]);
-        }
+void afficheTab(char tab[LIGNE][CARAC+1]){
+    int ligne, caract;
+    for(ligne=0; ligne<LIGNE; ligne++){
+        printf("%s", tab[ligne]);        
         printf("\n");
     }
 }
 
-void initTab(char t[LIGNE][CARAC]){
-    int ligne, car, hasard;
-    char temp;
-    for (ligne=0; ligne< LIGNE; ligne++){
-        for (car=0; car<CARAC-1; car++){
-            t[ligne][car]=(rand()%(122-97+1)+97);
+
+void initTab(char t[LIGNE][CARAC+1]){
+
+    int ligne, caract, nbCaractTemp;
+    for (ligne=0; ligne<LIGNE; ligne++){
+        nbCaractTemp=(rand()%(CARAC-5+1)+5);
+        for(caract=0; caract<nbCaractTemp; caract++){
+            t[ligne][caract]=(rand()%(122-97+1)+97);
         }
-        t[ligne][car]='\0';
+        if(nbCaractTemp<=CARAC){
+            for(; caract<=CARAC; caract++){
+                t[ligne][caract]='\0';
+            }
+        }
     }
 }
 
-void Tri_bulle_Croissant(char t[LIGNE][CARAC])
+
+
+void Tri_bulle_Croissant_nbCaract(char t[LIGNE][CARAC+1])
 {
     int D, d, c;
     bool ordre;
     ordre=false;
     c=0;
-
     for(D=0; D<LIGNE-1; D++){
         for(d=0; d<LIGNE-1-D; d++){
-            while(ordre==false && c<CARAC){
-                if(t[d][c]>t[d+1][c]){
+        while(ordre==false && c<CARAC){
+                if(strlen(t[d])>strlen(t[d+1])){
                     echange(t[d], t[d+1]);
                     ordre=true;
-                }else if(t[d][c]<t[d+1][c]){
+                }else if(strlen(t[d]) <= strlen(t[d+1])){
                     ordre=true;
-                }else{
-                    c++;
                 }
             }
         c=0;
         ordre=false;
         }
     }
+    Tri_bulle_Croissant_alpha(t);
 }
 
-void echange (char a[CARAC], char b[CARAC]){
-    char temp[CARAC];
+void Tri_bulle_Croissant_alpha(char t[LIGNE][CARAC+1])
+{
+    int D, d, c;
+    bool ordre;
+    ordre=false;
+    c=0;
+    for(D=0; D<LIGNE-1; D++){
+        for(d=0; d<LIGNE-1-D; d++){
+            if(strlen(t[d]) == strlen(t[d+1])){
+                while(ordre==false && c<CARAC){
+                    if(t[d][c]>t[d+1][c]){
+                        echange(t[d],t[d+1]);
+                        ordre=true;
+                    }else if(t[d][c]<t[d+1][c]){
+                        ordre=true;
+                    }else{
+                        c++;
+                    }
+                }
+            }
+            c=0;
+            ordre=false;
+        }
+    }
+}
+void echange (char a[CARAC+1], char b[CARAC+1]){
+    char temp[CARAC+1];
     strcpy(temp,a);
     strcpy(a,b);
     strcpy(b,temp);
